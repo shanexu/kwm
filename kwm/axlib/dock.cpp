c091572697d9fd2e498b25b979940917376a23f1
koekeishiya <aasvi93@hotmail.com>
2016-09-04 21:05:25 +0200

    fix awkward indentation

--- kwm/axlib/dock.cpp
+++ kwm/axlib/dock.cpp
@@ -12,24 +12,24 @@ extern "C" CGError CGSSetWindowLevel(CGSConnectionID Connection, CGWindowID Wind
 bool AXLibHijackUniversalOwner(CGSConnectionID Connection)
 {
     size_t Length = 0;
-	struct kinfo_proc *Result = NULL;
-	int Name[] = { CTL_KERN, KERN_PROC, KERN_PROC_ALL, 0 };
+    struct kinfo_proc *Result = NULL;
+    int Name[] = { CTL_KERN, KERN_PROC, KERN_PROC_ALL, 0 };
 
-	if((sysctl(Name, 3, NULL, &Length, NULL, 0)) ||
+    if((sysctl(Name, 3, NULL, &Length, NULL, 0)) ||
        ((Result = (kinfo_proc *) malloc(Length)) == NULL) ||
        (sysctl(Name, 3, Result, &Length, NULL, 0)))
     {
         return false;
     }
 
-	size_t Count = Length / sizeof(struct kinfo_proc);
-	struct kinfo_proc *Current = Result;
-	while(Count--)
+    size_t Count = Length / sizeof(struct kinfo_proc);
+    struct kinfo_proc *Current = Result;
+    while(Count--)
     {
-		if(strcmp(Current->kp_proc.p_comm, "Dock") == 0)
-			kill(Current->kp_proc.p_pid, SIGKILL);
+        if(strcmp(Current->kp_proc.p_comm, "Dock") == 0)
+            kill(Current->kp_proc.p_pid, SIGKILL);
 
-		Current++;
+        Current++;
     }
 
     CGSSetUniversalOwner(Connection);
