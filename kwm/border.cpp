f000040935b0a0baf2173b89ad81be6b934677ea
koekeishiya <aasvi93@hotmail.com>
2016-09-06 16:13:37 +0200

    #456 - make sure handle is valid

--- kwm/border.cpp
+++ kwm/border.cpp
@@ -1,11 +1,64 @@
 #include "border.h"
+#include "window.h"
 #include "axlib/axlib.h"
 
+#define internal static
+#define local_persist static
+
 extern kwm_hotkeys KWMHotkeys;
+extern kwm_path KWMPath;
+
+internal void
+OpenBorder(kwm_border *Border)
+{
+    local_persist std::string OverlayBin = KWMPath.FilePath + "/kwm-overlay";
+    DEBUG("Kwm: popen " << OverlayBin);
+
+    Border->Handle = popen(OverlayBin.c_str(), "w");
+    if(!Border->Handle)
+        Border->Enabled = false;
+}
+
+void CloseBorder(kwm_border *Border)
+{
+    if(Border->Handle)
+    {
+        local_persist std::string Terminate = "quit\n";
+        fwrite(Terminate.c_str(), Terminate.size(), 1, Border->Handle);
+        fflush(Border->Handle);
+        pclose(Border->Handle);
+        Border->Handle = NULL;
+    }
+}
+
+void ClearBorder(kwm_border *Border)
+{
+    if(Border->Handle)
+    {
+        local_persist std::string Command = "clear\n";
+        fwrite(Command.c_str(), Command.size(), 1, Border->Handle);
+        fflush(Border->Handle);
+    }
+}
+
+internal inline void
+RefreshBorder(kwm_border *Border, ax_window *Window)
+{
+    std::string Command = "x:" + std::to_string(Window->Position.x) + \
+                          " y:" + std::to_string(Window->Position.y) + \
+                          " w:" + std::to_string(Window->Size.width) + \
+                          " h:" + std::to_string(Window->Size.height) + \
+                          " " + Border->Color.Format + \
+                          " s:" + std::to_string(Border->Width);
+
+    Command += (Border->Radius != -1 ? " rad:" + std::to_string(Border->Radius) : "") + "\n";
+    fwrite(Command.c_str(), Command.size(), 1, Border->Handle);
+    fflush(Border->Handle);
+}
 
 void UpdateBorder(kwm_border *Border, ax_window *Window)
 {
-    if(Border)
+    if(Border && Border->Enabled)
     {
         if(Window)
         {
@@ -13,11 +66,11 @@ void UpdateBorder(kwm_border *Border, ax_window *Window)
                (!KWMHotkeys.ActiveMode->Color.Format.empty()))
                 Border->Color = KWMHotkeys.ActiveMode->Color;
 
-            OpenBorder(Border);
-            if(!Border->Enabled)
-                CloseBorder(Border);
+            if(!Border->Handle)
+                OpenBorder(Border);
 
-            RefreshBorder(Border, Window);
+            if(Border->Handle)
+                RefreshBorder(Border, Window);
         }
         else
         {
--- kwm/border.h
+++ kwm/border.h
@@ -2,65 +2,10 @@
 #define BORDER_H
 
 #include "types.h"
-#include "window.h"
 #include "axlib/axlib.h"
 
-#define local_persist static
-extern kwm_path KWMPath;
-
-inline void
-OpenBorder(kwm_border *Border)
-{
-    if(Border->Enabled && !Border->Handle)
-    {
-        local_persist std::string OverlayBin = KWMPath.FilePath + "/kwm-overlay";
-        DEBUG("Kwm: popen " << OverlayBin);
-
-        Border->Handle = popen(OverlayBin.c_str(), "w");
-        if(!Border->Handle)
-            Border->Enabled = false;
-    }
-}
-
-inline void
-CloseBorder(kwm_border *Border)
-{
-    if(Border->Handle)
-    {
-        local_persist std::string Terminate = "quit\n";
-        fwrite(Terminate.c_str(), Terminate.size(), 1, Border->Handle);
-        fflush(Border->Handle);
-        pclose(Border->Handle);
-        Border->Handle = NULL;
-    }
-}
-
-inline void
-ClearBorder(kwm_border *Border)
-{
-    if(Border->Handle)
-    {
-        local_persist std::string Command = "clear\n";
-        fwrite(Command.c_str(), Command.size(), 1, Border->Handle);
-        fflush(Border->Handle);
-    }
-}
-
-inline void
-RefreshBorder(kwm_border *Border, ax_window *Window)
-{
-    std::string Command = "x:" + std::to_string(Window->Position.x) + \
-                          " y:" + std::to_string(Window->Position.y) + \
-                          " w:" + std::to_string(Window->Size.width) + \
-                          " h:" + std::to_string(Window->Size.height) + \
-                          " " + Border->Color.Format + \
-                          " s:" + std::to_string(Border->Width);
-
-    Command += (Border->Radius != -1 ? " rad:" + std::to_string(Border->Radius) : "") + "\n";
-    fwrite(Command.c_str(), Command.size(), 1, Border->Handle);
-    fflush(Border->Handle);
-}
-
+void CloseBorder(kwm_border *Border);
+void ClearBorder(kwm_border *Border);
 void UpdateBorder(kwm_border *Border, ax_window *Window);
 
 #endif
